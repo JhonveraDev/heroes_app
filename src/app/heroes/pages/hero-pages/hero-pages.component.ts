fed114d01de561c706873feb3562325be750cab4
Anderson Vera <70354869+JhonveraDev@users.noreply.github.com>
2023-11-14 16:25:40 -0500

    route management

---
+++ src/app/heroes/pages/hero-pages/hero-pages.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-pages',
+  templateUrl: './hero-pages.component.html',
+  styleUrls: ['./hero-pages.component.css']
+})
+export class HeroPagesComponent {
+
+}
